59cc252317cea5518cc7faed27007d416dd91365
Sidibe <Elstevehcks@gmail.com>
2023-02-07 20:08:51 -0500

    Create test file to understand fork and pipe

---
+++ testForkPipe.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // Program create two processes P1 and P2
+    // P1 takes a string and passes to P2
+    // P2 concatenate string provided by P1 without string func
+    // P2 sends concatenated string to P1
+    // P1 prints results given by P2
+
+    return 0;
+}
